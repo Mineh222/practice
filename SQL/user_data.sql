3b227a436bd6f7e55b836e4d82ce8e8a2ca37830
Mineh Gharabegi <minehgharabegi@gmail.com>
2022-10-28 19:24:17 -0700

    created user data tables

--- SQL/user_data.sql
+++ SQL/user_data.sql
@@ -1,2 +1,23 @@
 -- Pipe the user_data.sql seed file into the "interview" database.
 -- psql -d interview -U Mineh < user_data.sql;
+
+CREATE TABLE users (
+    user_number INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE career_info (
+    info_number INT NOT NULL,
+    info_type VARCHAR(50) NOT NULL,
+    info_name VARCHAR(50) NOT NULL,
+    info_amount_type VARCHAR(50) NOT NULL,
+    info_amount INT NOT NULL
+),
+
+CREATE TABLE user_data (
+    user_number INT NOT NULL,
+    info_number INT NOT NULL,
+    date_updated DATE NOT NULL,
+    quarter VARCHAR(50) NOT NULL,
+)
