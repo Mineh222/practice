098ca648f95401bd666200fbf647951cb1e19308
Mineh Gharabegi <minehgharabegi@gmail.com>
2022-10-29 21:47:03 -0700

    last query done

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -7,6 +7,14 @@ SELECT CONCAT(users.first_name,' ',users.last_name) AS "full_name", date_part('y
 FROM user_data
 INNER JOIN users ON (user_data.user_number = users.user_number);
 
+--   full_name   | year
+-- --------------+------
+--  Minnie Mouse | 2015
+--  Yosemite Sam | 2016
+--  Minnie Mouse | 2016
+--  Bugs Bunny   | 2016
+--  Yosemite Sam | 2014
+
 /*
 Modify the SQL query from question 1 so that it returns the
 fiscal year instead of the calendar year.
@@ -21,6 +29,14 @@ SELECT
 FROM user_data
 INNER JOIN users ON (user_data.user_number = users.user_number);
 
+--   full_name   | fiscal_year
+-- --------------+-------------
+--  Minnie Mouse |        2016
+--  Yosemite Sam |        2017
+--  Minnie Mouse |        2017
+--  Bugs Bunny   |        2017
+--  Yosemite Sam |        2015
+
 
 /*
 Write a SQL query (a single query) that will return the
@@ -30,7 +46,10 @@ first and last name of the users who don’t have any saved occupations.
 SELECT users.first_name, users.last_name
 FROM users
 LEFT JOIN user_data ON (users.user_number = user_data.user_number)
-WHERE user_data.info_number
-IN (SELECT career_info.info_number
-    FROM career_info
-    WHERE career_info.info_type != "Occupation");
+WHERE user_data.info_number IS NULL OR user_data.info_number
+NOT IN (SELECT career_info.info_number FROM career_info);
+
+--  first_name | last_name
+-- ------------+-----------
+--  Daisy      | Duck
+--  Fred       | Flinstone
